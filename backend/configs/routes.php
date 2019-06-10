aa74798c89a33008f94351eb6ce79aae0c836797
CS59021243 <39623506+Noppanai003@users.noreply.github.com>
2019-06-10 23:57:19 +0700

    Update routes.php

--- backend/configs/routes.php
+++ backend/configs/routes.php
@@ -2,5 +2,6 @@
         // Array ที่เป็น routes     path file
 $routes['/api/member']['GET'] = './member/get.php'; // เรียกใช้งาน
 $routes['/api/member']['POST'] = './member/post.php'; // สร้างตัวเรียก และลงทะเบียน rount
-$routes['/api/member']['PUT'] = './member/put.php';
-$routes['/api/member']['DELETE'] = './member/delete.php';
+// เตรียมค่า
+// $routes['/api/member']['PUT'] = './member/put.php';
+// $routes['/api/member']['DELETE'] = './member/delete.php';
